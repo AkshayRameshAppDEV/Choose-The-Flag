daf2868dc56be6175f77cb2f1d0f1b18b81f01b2
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-11-08 23:12:52 -0600

    Added game with challenge

--- Choose The Flag/Choose The Flag/Controller/ViewController.swift
+++ Choose The Flag/Choose The Flag/Controller/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     let flagImages = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     var actualAnswer: Int? = nil
     var actualAnswerSet = [String]()
+    var score = 0
+    var currentQuestion = 1
 
     
     override func viewDidLoad() {
@@ -40,25 +42,42 @@ class ViewController: UIViewController {
         bottomFlag.setBackgroundImage(UIImage(named: flagImages[randomNumber3]), for: .normal)
         actualAnswer = randomElementFromRandomArray
         title = flagImages[randomElementFromRandomArray].uppercased()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Score: \(self.score)", style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "\(self.currentQuestion) of 10", style: .plain, target: self, action: nil)
     }
     
     @IBAction func clickFlag(_ sender: UIButton) {
         let chosenAnswer = sender.tag
+        self.currentQuestion += 1
         if actualAnswerSet[chosenAnswer] == flagImages[actualAnswer!] {
-            showAlert(alertTitle: "Correct", alertMessage: "Your chosen Answer is right!")
+            score += 1
+            showAlert(alertTitle: "Correct", alertMessage: "Your chosen Answer is right!\n Score: \(self.score)")
         } else {
-            showAlert(alertTitle: "Wrong", alertMessage: "Your chosen Answer is wrong, because you chose \(actualAnswerSet[chosenAnswer].uppercased())")
+            score -= 1
+            showAlert(alertTitle: "Wrong", alertMessage: "Your chosen Answer is wrong, because you chose \(actualAnswerSet[chosenAnswer].uppercased()) \n Score: \(self.score)")
         }
     }
     
     func showAlert(alertTitle: String, alertMessage: String) {
-        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in
-            self.actualAnswer = 0
-            self.actualAnswerSet = []
-            self.pick3RandomFlagsAndTitle()
-        }))
-        self.present(alert, animated: true, completion: nil)
+        if(self.currentQuestion <= 10) {
+            let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in
+                self.actualAnswer = 0
+                self.actualAnswerSet = []
+                self.pick3RandomFlagsAndTitle()
+            }))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Game Over", message: "Your score \(self.score)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Restart the game", style: .default, handler: { _ in
+                self.score = 0
+                self.currentQuestion = 1
+                self.actualAnswer = 0
+                self.actualAnswerSet = []
+                self.pick3RandomFlagsAndTitle()
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
 }
